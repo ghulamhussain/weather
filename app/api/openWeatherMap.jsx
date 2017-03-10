c0d6aaa69d020e5bd1fec2b15ab6e49bba5e17b0
ghulamhussain <ghulamhussain23@gmail.com>
2017-03-10 15:45:09 +0500

    URLChange

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=c4e735ea8bd7e7b6dc8368c752517b2d&units=metric';
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=3aa6c4c1c376a0f76d52b90aea73b610&units=metric';
 
 module.exports = {
   getTemp: function (location) {
